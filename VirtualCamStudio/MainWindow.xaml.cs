c763087ab365f40619881cba3925323e2e681aa1
David Lee <detalaim@gmail.com>
2026-06-28 02:42:40 +0000

    Initial VirtualCam Studio project

---
+++ VirtualCamStudio/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace VirtualCamStudio
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Copy;
+            e.Handled = true;
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+                return;
+
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            if (files.Length == 0)
+                return;
+
+            string file = files[0];
+
+            string ext = Path.GetExtension(file).ToLower();
+
+            if (ext == ".jpg" ||
+                ext == ".jpeg" ||
+                ext == ".png" ||
+                ext == ".bmp")
+            {
+                LoadImage(file);
+            }
+        }
+
+        private void LoadImage(string path)
+        {
+            BitmapImage bitmap = new BitmapImage();
+
+            bitmap.BeginInit();
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.UriSource = new Uri(path);
+            bitmap.EndInit();
+
+            PreviewImage.Source = bitmap;
+            DropText.Visibility = Visibility.Collapsed;
+        }
+    }
+}
